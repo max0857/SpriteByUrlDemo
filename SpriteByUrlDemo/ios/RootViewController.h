fbb7fd79576afa456916a0d9bc5526eee7f5546d
max0857 <Mac06@YanKaiPengs-iMac.local>
2014-10-20 23:26:32 +0800

    创建项目

---
+++ SpriteByUrlDemo/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SpriteByUrlDemoAppController.h
+//  SpriteByUrlDemo
+//
+//  Created by yankai.peng on 14/10/20.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
